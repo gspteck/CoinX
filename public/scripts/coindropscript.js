312a846cd1188f7bd2cf2b685ec3f1bfe982305d
GSPTeck <66552124+gspteck@users.noreply.github.com>
2025-05-02 13:22:17 +0200

    SEO optimization

--- public/scripts/coindropscript.js
+++ public/scripts/coindropscript.js
@@ -1,4 +1,6 @@
-console.log("coindropscript.js loaded");
 function openGooglePlayStore() {
 	window.open("https://play.google.com/store/apps/details?id=com.gspteck.coindrop", "_blank");
+}
+function openAmazonStore() {
+	window.open("", "_blank");
 }
